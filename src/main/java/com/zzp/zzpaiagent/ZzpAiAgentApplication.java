424d2f3a5bac2cc5aca7c82320451674cd950ec8
zzp <2664871975@qq.com>
2026-02-26 14:02:49 +0800

    begin zzp-ai-agent

---
+++ src/main/java/com/zzp/zzpaiagent/ZzpAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.zzp.zzpaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzpAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZzpAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zzp/zzpaiagent/ZzpAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzp.zzpaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzpAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
